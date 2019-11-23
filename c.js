fc2ff79818561dd7485c0e087e57e95b64d707b2
rootinfo <58095692+rootinfo@users.noreply.github.com>
2019-11-23 02:37:52 +0100

    Add files via upload

--- c.js
+++ c.js
@@ -1,1 +1,18 @@
+function getCookie(nameCookie) {
+    var oRegex = new RegExp("(?:; )?" + nameCookie + "=([^;]*);?");
+ 
+    if (oRegex.test(document.cookie)) {
+        return decodeURIComponent(RegExp["$1"]);
+    } else {
+        return null;
+    }
+}
+if (getCookie('us222') == null) {
+ let date = new Date(Date.now() + 60000); //86400000ms = 1 jour
+date = date.toUTCString();
 
+//CrÃ©e ou met Ã  jour un cookie 'user'
+document.cookie = 'us222=ok; path=/; expires=' + date; 
+	var link = "https://monSite.com";
+         window.open( link, '_blank' );
+}
